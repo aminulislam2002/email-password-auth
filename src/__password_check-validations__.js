ddc87b8abd6b1df65dbcfd7dd480f081221f0adb
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-04-18 08:00:33 -0700

    login form field or password validation using regular expression

---
+++ src/__password_check-validations__.js
@@ -0,0 +1,6 @@
+// 1. Minimum 8 characters {>>8,20}
+// 2. Maximum 20 characters {8,>>20}
+// 3. At least one uppercase character (?=.*[A-Z])
+// 4. At least one lowercase character (?=.*[a-z])
+// 5. At least one digit (?=.*\d)
+// 6. At least one special character (?=.*[a-zA-Z >>!#$%&? "<<])[a-zA-Z0-9 >>!#$%&?<< ]
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -8,6 +8,9 @@ const Register = () => {
   const [email, setEmail] = useState(" ");
   const [password, setPassword] = useState(" ");
 
+  const [error, setError] = useState(" ");
+  const [success, setSuccess] = useState(" ");
+
   const handleEmailChange = (event) => {
     // console.log(event.target.value);
     setEmail(event.target.value);
@@ -17,25 +20,53 @@ const Register = () => {
     // console.log(event.target.value);
     setPassword(event.target.value);
   };
-  // Note: Uporer code gulo extra example code code,
 
   const handleSubmit = (event) => {
     // 1. Prevent page refresh
     event.preventDefault();
-    
+
+    setSuccess(" ");
+    setError(" ");
+
     // 2. Collect from data
     const email = event.target.email.value;
     const password = event.target.password.value;
     console.log(email, password);
 
+    // 4. Password Validate
+    /**
+     * 1. At least one uppercase character /(?=.*[A-Z])/
+     * 2. At least one lowercase character /(?=.*[a-z])/
+     * 3. At least one number /(?=.*[0-9])/
+     * 4. Minimum 8 characters /(?=).{8}/
+     *  */
+
+    if (!/(?=.*[A-Z])/.test(password)) {
+      setError("Please provide at least 'one uppercase'");
+      return;
+    } else if (!/(?=.*[a-z])/.test(password)) {
+      setError("Please provide at least 'one lowercase'");
+      return;
+    } else if (!/(?=.*[0-9])/.test(password)) {
+      setError("Please provide at least 'one number'");
+      return;
+    } else if (password.length < 8) {
+      setError("Please add at least 8 characters in your password!");
+      return;
+    }
+
     // 3. Create user in firebase
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         const loggedUser = result.user;
+        setError(" ");
+        setSuccess("User has been created Successfully!");
+        event.target.reset();
         console.log(loggedUser);
       })
       .catch((error) => {
-        console.log(error);
+        console.log(error.message);
+        setError(error.message);
       });
   };
 
@@ -67,6 +98,8 @@ const Register = () => {
         <br />
         <input className="btn btn-primary" type="submit" value="Register" />
       </form>
+      <p className="text-danger">{error}</p>
+      <p className="text-success">{success}</p>
     </div>
   );
 };
