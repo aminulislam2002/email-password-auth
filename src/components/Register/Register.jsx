2945fdd8da8fd5d5bd4bc776c838e8ba55f5d73e
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-04-18 02:54:25 -0700

    form, input, change, blur, submit, preventDefault

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Register.css";
 
 const Register = () => {
+  const [email, setEmail] = useState(" ");
+  const [password, setPassword] = useState(" ");
+
+  const handleEmailChange = (event) => {
+    // console.log(event.target.value);
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordBlur = (event) => {
+    // console.log(event.target.value);
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email, password);
+  };
+
   return (
     <div>
       <h4>Please Register</h4>
-      <form>
-        <input type="email" name="email" id="email" placeholder="Your Email..." />
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder="Your Email..." />
         <br />
-        <input type="password" name="password" id="password" placeholder="Your Password..." />
+        <input onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder="Your Password..." />
         <br />
-        <input type="submit" value="Register" />
+        <input type="submit" value="Register" id="register" />
       </form>
     </div>
   );
